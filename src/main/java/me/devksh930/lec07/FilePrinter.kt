57b544b44e0a68b1c32701a0020241477693dba7
devksh930 <ksh930@icloud.com>
2022-10-03 20:55:35 +0900

    코틀린 예외처리

---
+++ src/main/java/me/devksh930/lec07/FilePrinter.kt
@@ -0,0 +1,21 @@
+package me.devksh930.lec07
+
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+
+class FilePrinter {
+    fun readFIle() {
+        val currentFile = File(".")
+        val file = File(currentFile.absolutePath + "/a.txt")
+        val reader = BufferedReader(FileReader(file))
+        println(reader.readLine())
+        reader.close()
+    }
+
+    fun readFile2(path: String) {
+        BufferedReader(FileReader(path)).use { reader ->
+            println(reader.readLine())
+        }
+    }
+}
---
+++ src/main/java/me/devksh930/lec07/Lec07Main.kt
@@ -0,0 +1,17 @@
+package me.devksh930.lec07
+
+fun parseIntOrThrow(str: String): Int {
+    try {
+        return str.toInt()
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("주어진 ${str}는 숫자가 아닙니다")
+    }
+}
+
+fun paraseIntOrThrow(str: String): Int? {
+    return try {
+        str.toInt()
+    } catch (e: NumberFormatException) {
+         null
+    }
+}
